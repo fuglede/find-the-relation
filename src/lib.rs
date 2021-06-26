10822f928a346715c1951806d3a28dcb37614dd4
Søren Fuglede Jørgensen <github@fuglede.dk>
2021-06-26 13:54:54 +0000

    Add game type and style frontend

--- src/lib.rs
+++ src/lib.rs
@@ -19,34 +19,217 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub struct Game {
-    qs: Vec<Complex<f64>>,
-    groups: Vec<Group>,
-    evaluated: Vec<[Complex<f64>; 9]>
+    levels: Vec<Level>,
+    qs: Vec<Vec<String>>,
+    level_descriptions: Vec<String>,
+    active_level: usize
 }
 
 #[wasm_bindgen]
 impl Game {
-    fn new(qs: Vec<Complex<f64>>) -> Game {
-        let groups: Vec<Group> = qs.iter().map(Group::new).collect();
+    pub fn new() -> Game {
+        let mut levels = vec![];
+        let mut qs = vec![];
+        let mut level_descriptions = vec![];
+
+        // Level 1; q = 1
+        levels.push(Level::new(vec![Complex::new(1.0, 0.0)]));
+        qs.push(vec!["1".to_owned()]);
+        level_descriptions.push(
+            "In this first level, we will find a solution for 𝑞 = 1. That is,
+            each of the four directions correspond to multiplication by one of
+            the four matrices given in the rules in which all instances of 𝑞 have
+            been replaced with 1. This is the simplest case of all of them. Find a
+            solution by using the four buttons below, or by using the keyboard
+            shortcuts. Use the reset button to get back to where you started. Remember
+            that the level is completed when you find a path to the identity matrix."
+            .to_owned());
+        
+        // Level 2; q = -1
+        levels.push(Level::new(vec![Complex::new(-1.0, 0.0)]));
+        qs.push(vec!["−1".to_owned()]);
+        level_descriptions.push(
+            "Hopefully the first level wasn't too bad. In this next one, we use
+            𝑞 = −1 instead. This will make things a bit harder. Pay close attention
+            to how the entries of the matrix change in your quest to find a path
+            to the identity matrix. What's the shortest path you can find?"
+            .to_owned());
+        
+        // Level 3; q = i
+        levels.push(Level::new(vec![Complex::new(0.0, 1.0)]));
+        qs.push(vec!["𝑖".to_owned()]);
+        level_descriptions.push(
+            "In the first two levels, 𝑞 was a real number. In this one, we consider
+            𝑞 = 𝑖, the imaginary unit satisfying 𝑖² = −1. Can you still find a path?"
+            .to_owned());
+        
+        // Level 4: q = 1, -1, i
+        levels.push(Level::new(vec![Complex::new(1.0, 0.0), Complex::new(-1.0, 0.0), Complex::new(0.0, 1.0)]));
+        qs.push(vec!["1".to_owned(), "−1".to_owned(), "𝑖".to_owned()]);
+        level_descriptions.push(
+            "So far, we have tried to find paths for individual values of 𝑞. In this
+            one, we have to find a path that works for all previously considered values
+            at the same time. The level is completed when all three matrices are the
+            identity matrix."
+            .to_owned());
+        
+        // Level 5: q = exp(2*pi*i/3)
+        levels.push(Level::new(vec![Complex::from_polar(1.0, 2.0 * PI / 3.0)]));
+        qs.push(vec!["exp(2π𝑖/3)".to_owned()]);
+        level_descriptions.push(
+            "We continue our journey into the complex plane, this time with a value
+            of 𝑞 that's neither real or imaginary but still lives on the unit circle.
+            At this point, numerical errors will start to show up, and we just require
+            that the result is sufficiently close to the identity matrix."
+            .to_owned());
+        
+        // Level 6: q = exp(2*pi*i/5)
+        levels.push(Level::new(vec![Complex::from_polar(1.0, 2.0 * PI / 5.0)]));
+        qs.push(vec!["exp(2π𝑖/5)".to_owned()]);
+        level_descriptions.push(
+            "Let's do another one on the unit circle."
+            .to_owned());
+        
+        // Level 7: q = exp(2*pi*i/3), exp(2*pi*i/5)
+        levels.push(Level::new(vec![
+            Complex::from_polar(1.0, 2.0 * PI / 3.0),
+            Complex::from_polar(1.0, 2.0 * PI / 5.0)]));
+        qs.push(vec!["exp(2π𝑖/3)".to_owned(), "exp(2π𝑖/5)".to_owned()]);
+        level_descriptions.push(
+            "Can you complete both of the previous levels at once?"
+            .to_owned());
+
+        // Level 8; q = 2
+        levels.push(Level::new(vec![Complex::new(2.0, 0.0)]));
+        qs.push(vec!["2".to_owned()]);
+        level_descriptions.push(
+            "So far, all of our values of 𝑞 have been somewhere on the unit
+            circle. Let's move on to ones that aren't, starting with 𝑞 = 2."
+            .to_owned());
+
+        // Level 9; q = 3
+        levels.push(Level::new(vec![Complex::new(3.0, 0.0)]));
+        qs.push(vec!["3".to_owned()]);
+        level_descriptions.push(
+            "Final level! As our final challenge, we will consider 𝑞 = 3.
+            It is an open question of mathematics whether or not this one is
+            possible. If you find a solution, or if you find that none exist,
+            please get in touch."
+            .to_owned());
+
+        let active_level = 0;
+        Game { levels, qs, level_descriptions, active_level }
+    }
+
+    pub fn change_level(&mut self, i: usize) {
+        self.active_level = i;
+    }
+
+    pub fn active_level(&self) -> usize {
+        self.active_level
+    }
+
+    pub fn level_description(&self) -> String {
+        self.level_descriptions[self.active_level].clone()
+    }
+
+    pub fn qs(&self) -> Array {
+        let level_qs = self.qs[self.active_level].clone();
+        let length = level_qs.len();
+        let arr = Array::new_with_length(length as u32);
+        for i in 0..length {
+            arr.set(i as u32, JsValue::from_str(&format!("{}", level_qs[i])));
+        }
+        arr
+    }
+
+    pub fn push(&mut self, direction: Direction) {
+        self.levels[self.active_level].push(direction);
+    }
+
+    pub fn reset(&mut self) {
+        self.levels[self.active_level].reset();
+    }
+
+    pub fn evaluated(&self) -> Array {
+        self.levels[self.active_level].evaluated()
+    }
+
+    pub fn evaluation_is_trivial(&self) -> Array {
+        self.levels[self.active_level].evaluation_is_trivial()
+    }
+
+    pub fn is_solved(&self) -> bool {
+        self.levels[self.active_level].is_solved()
+    }
+
+    pub fn word(&self) -> String {
+        self.levels[self.active_level].word()
+    }
+}
+
+#[wasm_bindgen]
+pub struct Level {
+    qs: Vec<Complex<f64>>,
+    groups: Vec<Group>,
+    word: Vec<Direction>,
+    evaluated: Vec<[Complex<f64>; 9]>
+}
+
+#[wasm_bindgen]
+impl Level {
+    fn new(qs: Vec<Complex<f64>>) -> Level {
+        let groups: Vec<Group> = Self::make_groups(&qs);
         let evaluated = groups.iter().map(
             |g| g.flatten()).collect();
-        Game {
+        let word = vec![];
+        Level {
             qs,
             groups,
+            word,
             evaluated
         }
     }
 
-    pub fn easy() -> Game {
-        Self::new(vec![Complex::new(0.25, 0.0),
-            Complex::from_polar(1.0, PI/3.0)])
+    fn make_groups(qs: &Vec<Complex<f64>>) -> Vec<Group> {
+        qs.iter().map(Group::new).collect()
+    }
+
+    pub fn easy() -> Level {
+        Self::new(vec![
+            Complex::new(1.0, 0.0),
+            Complex::new(-1.0, 0.0),
+            Complex::new(0.0, 1.0),
+        ])
     }
 
     pub fn push(&mut self, direction: Direction) {
         for i in 0..self.groups.len() {
             self.groups[i].push(&direction);
-            self.evaluated[i] = self.groups[i].flatten();
         }
+        let last_is_opposite = self.word.len() != 0 &&
+            self.word.last().unwrap() == &match direction {
+            Direction::North => Direction::South,
+            Direction::South => Direction::North,
+            Direction::East => Direction::West,
+            Direction::West => Direction::East
+        };
+        if last_is_opposite {
+            self.word.pop();
+        } else {
+            self.word.push(direction);
+        }
+        self.update_evaluated();
+    }
+
+    pub fn reset(&mut self) {
+        self.groups = Self::make_groups(&self.qs);
+        self.word = vec![];
+        self.update_evaluated();
+    }
+
+    fn update_evaluated(&mut self) {
+        self.evaluated = self.groups.iter().map(|g| g.flatten()).collect();
     }
 
     pub fn evaluated(&self) -> Array {
@@ -73,6 +256,15 @@ impl Game {
         arr
     }
 
+    pub fn word(&self) -> String {
+        self.word.iter().map(|d| match d {
+            Direction::North => 'N',
+            Direction::South => 'S',
+            Direction::East => 'E',
+            Direction::West => 'W',
+        }).collect::<String>()
+    }
+
     pub fn qs(&self) -> Array {
         let length = self.groups.len();
         let arr = Array::new_with_length(length as u32);
@@ -89,6 +281,10 @@ impl Game {
             arr.set(i as u32, JsValue::from_bool(self.groups[i].current_is_identity()));
         }
         arr
+    }
+
+    pub fn is_solved(&self) -> bool {
+        self.word.len() > 0 && self.groups.iter().all(|g| g.current_is_identity())
     }
 
     pub fn det(&self) -> Array {
@@ -117,5 +313,5 @@ fn f64toa(x: &f64) -> String {
 }
 
 fn ztoa(z: &Complex<f64>) -> String {
-    format!("{} + {}i", f64toa(&z.re), f64toa(&z.im))
+    format!("{} + {}𝑖", f64toa(&z.re), f64toa(&z.im))
 }
--- www/index.js
+++ www/index.js
@@ -1,62 +1,105 @@
 import { Game, Direction } from "find-the-relation";
 
-const easy = Game.easy();
-const word = document.getElementById("word");
+const game = Game.new();
+
+var menuItems = document.getElementsByClassName("menu-item");
+var gameViewActive = false;
+
+function navigate(i) {
+    console.log(i)
+    document.getElementById("rules-menu-item").style.backgroundColor = "#252525";
+    for (var j = 0; j < menuItems.length - 1; j++) {
+        document.getElementById("level-" + j).style.backgroundColor = "#252525";
+    }
+    if (i == 0) {
+        document.getElementById("landing").style.display = "none";
+        document.getElementById("rules").style.display = "";
+        document.getElementById("game").style.display = "none";
+        document.getElementById("rules-menu-item").style.backgroundColor = "#151515";
+        gameViewActive = false;
+    } else {
+        document.getElementById("landing").style.display = "none";
+        document.getElementById("rules").style.display = "none";
+        document.getElementById("game").style.display = "";
+        document.getElementById("level-header").textContent = document.getElementById("level-" + (i - 1)).innerText;
+        document.getElementById("level-" + (i - 1)).style.backgroundColor = "#151515";
+        game.change_level(i - 1);
+        gameViewActive = true;
+        update_game_view();
+    }
+}
+
+for (var i = 0; i < menuItems.length; i++) {
+    const j = i;  // Copy to avoid capturing.
+    menuItems[i].onclick = function() { navigate(j) };
+};
 
 window.addEventListener("keydown", keydownHandler, false);
 
 function keydownHandler(e) {
+    if (!gameViewActive) return;
     var keyCode = e.which;
-    if (keyCode == 38) {
+    if (keyCode == 38 || keyCode == 87) {  // Up arrow and w
         moveNorth();
-    } else if (keyCode == 40) {
+    } else if (keyCode == 40 || keyCode == 83) {  // Down arrow and s
         moveSouth();
-    } else if (keyCode == 39) {
+    } else if (keyCode == 39 || keyCode == 68) {  // Down arrow and d
         moveEast();
-    } else if (keyCode == 37) {
+    } else if (keyCode == 37 || keyCode == 65) {  // Down arrow and a
         moveWest();
+    } else if (keyCode == 82) {  // r
+        reset();
     }
 }
-
+  
 function moveNorth() {
-    move(Direction.North, "N", "S");
+    move(Direction.North);
 }
 
 function moveSouth() {
-    move(Direction.South, "S", "N");
+    move(Direction.South);
 }
 
 function moveEast() {
-    move(Direction.East, "E", "W");
+    move(Direction.East);
 }
 
 function moveWest() {
-    move(Direction.West, "W", "E");
+    move(Direction.West);
 }
 
-function move(direction, symbol, opposite) {
-    easy.push(direction);
-    if (word.textContent.slice(-1) != opposite) {
-        word.textContent += symbol;
-    } else {
-        word.textContent = word.textContent.substring(0, word.textContent.length - 1);
-    }
-    update_table();
+function reset() {
+    game.reset();
+    update_game_view();
+}
+
+function move(direction) {
+    game.push(direction);
+    update_game_view();
 }
 
 document.getElementById("north-button").addEventListener("click", moveNorth);
 document.getElementById("south-button").addEventListener("click", moveSouth);
 document.getElementById("east-button").addEventListener("click", moveEast);
 document.getElementById("west-button").addEventListener("click", moveWest);
+document.getElementById("reset-button").addEventListener("click", reset);
 
-function update_table() {
-    var evaluated = easy.evaluated();
-    var is_trivial = easy.evaluation_is_trivial();
+function update_game_view() {
+    var evaluated = game.evaluated();
+    var is_trivial = game.evaluation_is_trivial();
+    var qs = game.qs();
+
+    document.getElementById("completed").style.display = "none";
+    document.getElementById("description").textContent = game.level_description();
+    document.getElementById("word").textContent = game.word();
     document.getElementById("matrices").innerHTML = '';
     for (var i = 0; i < evaluated.length/9; i++) {
         var div = document.createElement("div");
+        var qtest = document.createTextNode("𝑞 = " + qs[i] + ":");
+        div.appendChild(qtest);
         div.style.marginTop = "20px";
-        div.style.backgroundColor = is_trivial[i] && word.textContent != "" ? "#77dd77" : "#ff6961";
+        div.style.backgroundColor = is_trivial[i] && word.textContent != "" ? "#77dd77" : "#333333";
+        div.style.color = is_trivial[i] && word.textContent != "" ? "#333333" : "#f5bc41";
         var tbl = document.createElement("table");
         div.appendChild(tbl);
         for (var j = 0; j < 3; j++) {
@@ -69,8 +112,10 @@ function update_table() {
         }
         document.getElementById("matrices").appendChild(div);
     }
-    document.getElementById("matrices").appendChild(document.createTextNode(easy.det()));
-    document.getElementById("matrices").appendChild(document.createElement("br"));
-    document.getElementById("matrices").appendChild(document.createTextNode(easy.tr()));
-
+    if (game.is_solved()) {
+        document.getElementById("completed").style.display = "";
+        let current_level = game.active_level();
+        document.getElementById("level-" + current_level + "-completed").innerHTML = "✅";
+        document.getElementById("level-header").textContent = document.getElementById("level-" + current_level).innerText;
+    }
 }
